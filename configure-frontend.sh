90d2b0452bde6cb1160dc507418fd43d19820374
Adam Charron <adam@charrondev.com>
2019-08-17 14:16:19 -0400

    Add configure scripts

---
+++ configure-frontend.sh
@@ -0,0 +1,4 @@
+# #!/bin/bash
+#
+mkdir $HOME/workspace
+echo 'TARGET_DIR="$HOME/workspace/frontend"' >> $BASH_ENV
---
+++ configure-php.sh
@@ -0,0 +1,4 @@
+# #!/bin/bash
+#
+mkdir $HOME/workspace
+echo 'TARGET_DIR="$HOME/workspace/php"' >> $BASH_ENV
