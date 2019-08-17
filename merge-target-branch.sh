bde9d2c38dfc545015c7c89d3549810d9c49d321
Adam Charron <adam@charrondev.com>
2019-08-17 14:13:56 -0400

    Make 1 target branch script

--- merge-target-branch.sh
+++ merge-target-branch.sh
@@ -5,6 +5,25 @@
 #
 # https://ideas.circleci.com/ideas/CCI-I-431
 set -eu -o pipefail
+
+# Fetch the target branch.
+PR_NUMBER=${CI_PULL_REQUEST//*pull\//}
+if [ -z "$PR_NUMBER" ]; then
+    echo "Not a pull request. Skipping this step."
+    exit
+fi
+
+if [[ -n ${PR_NUMBER} ]]
+then
+    sudo apt-get update && sudo apt-get install -y jq
+    url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls/$PR_NUMBER?access_token=$GITHUB_TOKEN"
+    target_branch=$(
+        curl "$url" | jq '.base.ref' | tr -d '"'
+    )
+    echo "Found target branch $target_branch".
+    echo "CUSTOM_TARGET_BRANCH='$target_branch'" >> $BASH_ENV
+fi
+
 cd $TARGET_DIR
 # Merge target branch back into ourselves if we need to.
 err=0
