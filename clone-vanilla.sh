1eebb38d3c9fa624532e729788c19f0e1e03025a
Adam Charron <adam@charrondev.com>
2019-08-17 15:38:38 -0400

    Add clone vanilla script

---
+++ clone-vanilla.sh
@@ -0,0 +1,24 @@
+# #!/bin/bash
+#
+# Clone the vanilla repo for running tests against it.
+set -e
+
+TARGET_DIR="$HOME/workspace/vanilla"
+
+# Clone the repo.
+git clone git@github.com:vanilla/vanilla.git $TARGET_DIR
+cd $TARGET_DIR
+
+$TARGET_BRANCH="master"
+
+$repo_target=($CUSTOM_TARGET_BRANCH || $CIRCLE_BRANCH)
+
+# When our target branch is a release branch
+# We want to use the same target branch
+if [[ $repo_target == "release/*" ]]
+then
+    git checkout $repo_target
+fi
+
+## Temporary workaround until we have our circle config on master
+git checkout feature/no-travis
